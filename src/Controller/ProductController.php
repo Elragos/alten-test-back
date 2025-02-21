39ced05ec9eee73dc8afca71de79b34e4358a5c3
Alexandre Maréchal <alexandre.marechal.59@gmail.com>
2025-02-21 16:00:03 +0100

    Fixing auto routing

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,9 +6,12 @@ use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
+    /** GET methods */
+
     #[Route('/product', name: 'product_show_all', methods: ['GET'])]
     public function showAll(EntityManagerInterface $entityManager): Response
     {
