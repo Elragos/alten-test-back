08ce8ff17a3ec459fe980bec1f7b79784b5cf04e
Alexandre Maréchal <alexandre.marechal.59@gmail.com>
2025-03-18 15:02:13 +0100

    Adding initialData.json in loading fixtures

--- src/DataFixture/AppFixtures.php
+++ src/DataFixture/AppFixtures.php
@@ -2,9 +2,12 @@
 
 namespace App\DataFixture;
 
+use App\Entity\Product;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 /**
@@ -17,36 +20,128 @@ class AppFixtures extends Fixture
      */
     private UserPasswordHasherInterface $passwordEncoder;
 
-    public function __construct(UserPasswordHasherInterface $passwordEncoder)
+    /**
+     * @var string Application config path.
+     */
+    private string $configPath;
+
+    public function __construct(
+        UserPasswordHasherInterface $passwordEncoder,
+        #[Autowire('%kernel.project_dir%/config')] string $configPath
+    )
     {
         $this->passwordEncoder = $passwordEncoder;
+        $this->configPath = $configPath;
     }
 
     /**
-     * Create initial data (admin user wish pwd 123456)
+     * Create initial data from JSON config file.
+     *
      * @param ObjectManager $manager Used DB manager.
      * @return void
      */
     public function load(ObjectManager $manager): void
     {
-        // Find if admin user already created
-        $admin = $manager->getRepository(User::class)->findOneBy([
-            "email" => "admin@admin.com"
+        // Load initial data
+        $initialDataPath = $this->configPath . '/initialData.json';
+        if (file_exists($initialDataPath))
+        {
+            $initialDataContent = file_get_contents($initialDataPath);
+            // Parse JSON
+            $initialData = json_decode($initialDataContent, true);
+
+            // For all users in initial data
+            foreach ($initialData["users"] as $userData) {
+                // Create it
+                $user = new User();
+                $user->setUsername($userData["username"])
+                    ->setFirstname($userData["firstname"])
+                    ->setEmail($userData["email"])
+                    ->setPassword(
+                        $this->passwordEncoder->hashPassword($user, $userData["password"])
+                    )
+                    ->setRoles([$userData["role"]]);
+
+                $this->createAccount($user, $manager);
+            }
+            // For all products in initial data
+            foreach ($initialData["products"] as $productData) {
+                // Create it
+                $product = new Product();
+                $product->setCode($productData["code"])
+                    ->setName($productData["name"])
+                    ->setDescription($productData["description"])
+                    ->setImage($productData["image"])
+                    ->setCategory($productData["category"])
+                    ->setPrice($productData["price"])
+                    ->setQuantity($productData["quantity"])
+                    ->setInternalReference($productData["internalReference"])
+                    ->setShellId($productData["shellId"])
+                    ->setInventoryStatus($productData["inventoryStatus"])
+                    ->setRating($productData["rating"])
+                    ->setCreatedAt(new \DateTimeImmutable())
+                    ->setUpdatedAt(new \DateTimeImmutable());
+
+                $this->createProduct($product, $manager);
+            }
+        }
+        else
+        {
+            throw new FileNotFoundException("Initial data file \"$initialDataPath\" does not exist.");
+        }
+
+        // Create default admin account
+        $this->createDefaultAdmin($manager);
+    }
+
+    /**
+     * Create user in DB if not exists.
+     *
+     * @param User $toCreate User to create.
+     * @param ObjectManager $manager Used DB manager
+     * @return void
+     */
+    private function createAccount(User $toCreate, ObjectManager $manager) : void
+    {
+        // Check if account exists
+        $exists = $manager->getRepository(User::class)->findOneBy([
+            "email" => $toCreate->getEmail()
         ]);
 
-        // If not
-        if ($admin == null) {
-            // Create it
-            $admin = new User();
-            $admin->setUsername('admin');
-            $admin->setFirstname('admin');
-            $admin->setEmail('admin@admin.com');
-            $admin->setPassword(
-                $this->passwordEncoder->hashPassword($admin, '123456')
-            );
-            $admin->setRoles(['ROLE_ADMIN']);
+        if ($exists == null) {
+            $manager->persist($toCreate);
+            $manager->flush();
+        }
+    }
 
-            $manager->persist($admin);
+    /**
+     * Create default admin account with password 123456 (if not already registered).
+     *
+     * @param ObjectManager $manager Used DB manager.
+     * @return void
+     */
+    private function createDefaultAdmin(ObjectManager $manager) : void
+    {
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setFirstname('admin');
+        $admin->setEmail('admin@admin.com');
+        $admin->setPassword(
+            $this->passwordEncoder->hashPassword($admin, '123456')
+        );
+        $admin->setRoles(['ROLE_ADMIN']);
+        $this->createAccount($admin, $manager);
+    }
+
+    private function createProduct(Product $toCreate, ObjectManager $manager) : void
+    {
+        // Check if account exists
+        $exists = $manager->getRepository(Product::class)->findOneBy([
+            "code" => $toCreate->getCode()
+        ]);
+
+        if ($exists == null) {
+            $manager->persist($toCreate);
             $manager->flush();
         }
     }
