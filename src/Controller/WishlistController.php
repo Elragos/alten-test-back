f8bb58681fb6e85a443b2dd1bf899c79a7651314
Alexandre Maréchal <alexandre.marechal.59@gmail.com>
2025-04-04 15:25:50 +0200

    Removing unused import in wishlist controller

--- src/Controller/WishlistController.php
+++ src/Controller/WishlistController.php
@@ -6,7 +6,6 @@ use App\Entity\Product;
 use App\Entity\Wishlist;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
