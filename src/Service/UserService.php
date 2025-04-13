a472b3f338adebc8ea0c06c50e8c4f09fc384991
Alexandre Maréchal <alexandre.marechal.59@gmail.com>
2025-04-13 15:38:08 +0200

    Fixing missing import

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use InvalidArgumentException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 /**
